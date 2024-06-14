d44d5b7d10899b0caf124a5b43b7d75398c500d9
gideonihonre <gideonihonre345@gmail.com>
2024-06-14 16:07:34 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react"
-
+import ButtonGradient from "./assets/assets/svg/ButtonGradient"
+import Button from "./components/Button"
 const App = () => {
-  return <div></div>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
+        <Button className="m-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  )
 }
 
 export default App
+// src\assets\assets\svg\ButtonGradient.jsx
