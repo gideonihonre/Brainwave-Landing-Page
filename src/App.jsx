8cc5828eb1f325ae176e723173126c122a9aea66
gideonihonre <gideonihonre345@gmail.com>
2024-06-17 22:51:47 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import ButtonGradient from "./assets/assets/svg/ButtonGradient"
 import Button from "./components/Button"
+import Header from "./components/Header"
 const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
-        <Button className="m-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
